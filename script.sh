21524e29e601a6728f8c4ec4817a1316d69092f6
Vionikk <61514541+Vionikk@users.noreply.github.com>
2021-04-13 23:36:01 +0300

    xdzfsdf

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
-#!/bin/bash  
+#!/bin/bash   
         ml icc
+	echo “AMENO”
 	echo “Optimization O1”
 	icc -restrict -O1 Neural.cpp -o simp1 
 	time ./ Neural.cpp                                                                                                                 
@@ -9,5 +10,6 @@
 	echo “Optimization O3”
 	icc -restrict -O3 -qopt-report=3 -qopt-report-phase=vec -D NOALIAS Neural.cpp -o simpvec3
 	time ./ Neural.cpp
-		
+	echo “YESSSS”
+	
 	cflis=`grep "flags" /proc/cpuinfo | head -1 | cut -d':' -f2 | cut -d" " -f2- | tr _ . | tr "a-z" "A-Z"
