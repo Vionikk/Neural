c0a72cac0903b9a258b164b8d2e341dc1e5ae02c
Vionikk <61514541+Vionikk@users.noreply.github.com>
2021-04-14 19:21:45 +0300

    Long long

--- Neural.cpp
+++ Neural.cpp
@@ -10,18 +10,18 @@ double func(long double x[], long double y) {
 }
 int main() {
     long double t_in1[3] = { 1, 0, 0 }, t_out1 = 0, w1 = -0.37543/*Случайные веса*/, out1;
-    long double t_in2[3] = { 1, 1, 0 }, t_out2 = 1, w2 = -0.37543/*Случайные веса*/, out2;
-    long double t_in3[3] = { 1, 1, 1 }, t_out3 = 1, w3 = -0.37543/*Случайные веса*/, out3;
-    for (long i1 = 0; i1 < 2147483646; i1++) {
+    //long double t_in2[3] = { 1, 1, 0 }, t_out2 = 1, w2 = -0.37543/*Случайные веса*/, out2;
+    //long double t_in3[3] = { 1, 1, 1 }, t_out3 = 1, w3 = -0.37543/*Случайные веса*/, out3;
+    for (unsigned long long i1 = 0; i1 < 18446744073709551614; i1++) {
         out1 = sigmoid(func(t_in1, w1));
         w1 += func(t_in1, (t_out1 - out1) * out1 * (1 - out1));
     }
-    for (long i2 = 0; i2 < 2147483645; i2++) {
+    /*for (unsigned long long i2 = 0; i2 < 18446744073709551613; i2++) {
         out2 = sigmoid(func(t_in2, w2));
         w2 += func(t_in2, (t_out2 - out2) * out2 * (1 - out2));
     }
-    for (long i3 = 0; i3 < 2147483644; i3++) {
+    for (unsigned long long i3 = 0; i3 < 18446744073709551611; i3++) {
         out3 = sigmoid(func(t_in3, w3));
         w3 += func(t_in3, (t_out3 - out3) * out3 * (1 - out3));
-    }
+    }*/
 }
