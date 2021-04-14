3e272223b73c0eb8cb5f731c7744d7a823b89c8c
Vionikk <61514541+Vionikk@users.noreply.github.com>
2021-04-14 15:08:11 +0300

    Update scenario_.sh

--- scenario_.sh
+++ scenario_.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 ml icc
-echo "g++ compilation with 0 flag:"
+echo "g++ compilation(0 flag):"
 g++ -O Neural.cpp -o test0
 time ./test0
 echo -e "\n"
 
-echo "g++ compilation with 01 flag:"
+echo "g++ compilation(01 flag):"
 g++ -O1 Neural.cpp -o test1
 time ./test1
 echo -e "\n"
 
-echo "g++ compilation with 02 flag:"
+echo "g++ compilation(02 flag):"
 g++ -O2 Neural.cpp -o test2
 time ./test2
 echo -e "\n"
 
-echo "g++ compilation with 03 flag:"
+echo "g++ compilation(03 flag):"
 g++ -O3 Neural.cpp -o test3
 time ./test3
 echo -e "\n"
 
-echo "g++ compilation with 0fast flag:"
+echo "g++ compilation(0fast flag):"
 g++ -Ofast Neural.cpp -o test4
 time ./test4
 echo -e "\n"
@@ -35,7 +35,7 @@ do
 icc -std=c++11 -x$flag -O2 Neural.cpp -o nexttest
 if [$? -eq 0]
 then
-echo "Extention is $flag"
+echo "Extention: $flag"
 time ./nexttest
 echo -e "\n"
 fi
