71d6a91d27080ad5e7cf83b852e4c018f729759e
Vionikk <61514541+Vionikk@users.noreply.github.com>
2021-04-14 12:04:40 +0300

    scenario_.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,42 @@
-#!/bin/bash   
-        ml icc
-	echo “AMENO”
-	echo “Optimization O1”
-	icc -restrict -O1 Neural.cpp -o simp1 
-	time ./ Neural.cpp                                                                                                                 
-	echo “Optimization O2”	
-	icc -restrict -O2 -qopt-report=2 -qopt-report-phase=vec -D NOALIAS Neural.cpp -o simpvec2     
-	time ./ Neural.cpp                                                         
-	echo “Optimization O3”
-	icc -restrict -O3 -qopt-report=3 -qopt-report-phase=vec -D NOALIAS Neural.cpp -o simpvec3
-	time ./ Neural.cpp
-	echo “YESSSS”
-	
-	cflis=`grep "flags" /proc/cpuinfo | head -1 | cut -d':' -f2 | cut -d" " -f2- | tr _ . | tr "a-z" "A-Z"
+#!/bin/bash
+
+ml icc
+echo "g++ compilation with 0 flag:"
+g++ -O Neural.cpp -o test0
+time ./test0
+echo -e "\n"
+
+echo "g++ compilation with 01 flag:"
+g++ -O1 Neural.cpp -o test1
+time ./test1
+echo -e "\n"
+
+echo "g++ compilation with 02 flag:"
+g++ -O2 Neural.cpp -o test2
+time ./test2
+echo -e "\n"
+
+echo "g++ compilation with 03 flag:"
+g++ -O3 Neural.cpp -o test3
+time ./test3
+echo -e "\n"
+
+echo "g++ compilation with 0fast flag:"
+g++ -Ofast Neural.cpp -o test4
+time ./test4
+echo -e "\n"
+
+grep 'flags' /proc/cpuinfo | uniq
+echo -e "\n"
+
+extn=(sse2 ssse3 sse4.2 avx)"
+for flag in ${extn[@]}
+do
+icc -std=c++11 -x$flag -O2 Neural.cpp -o nexttest
+if [$? -eq 0]
+then
+echo "Extention is $flag"
+time ./nexttest
+echo -e "\n"
+fi
+done
