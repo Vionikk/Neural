64fdd08f6ee4875d9480e8aa691e44fa9d3d4d87
Vionikk <61514541+Vionikk@users.noreply.github.com>
2021-05-16 14:33:50 +0300

    Update Neural.cpp

--- Neural.cpp
+++ Neural.cpp
@@ -1,68 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-// A utility function to swap two elements
-void swap(int* a, int* b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
+#include <iostream>
+#include <cmath>
+#include <time.h>
+//Vionikk
+double sigmoid(long double x) {
+    return 1 / (1 + std::exp(-x));
 }
- 
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
-int partition (int arr[], int low, int high)
-{
-    int pivot = arr[high]; // pivot
-    int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
- 
-    for (int j = low; j <= high - 1; j++)
-    {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot)
-        {
-            i++; // increment index of smaller element
-            swap(&arr[i], &arr[j]);
-        }
+double func(long double x[], long double y) {
+    return x[0] * y + x[1] * y + x[2] * y;
+}
+long double average(long double a, long double b, long double c, long double d){
+    long double t = 4.0;
+    return (a+b+c+d)/t;
+}
+int main() {
+    long double t_in1[3] = { 1, 0, 0 }, t_out1 = 1, w1 = -0.37543/*Випадкові ваги*/;
+    long double t_in2[3] = { 1, 1, 0 }, t_out2 = 1, w2 = -0.37543/*Випадкові ваги*/;
+    long double t_in3[3] = { 1, 1, 1 }, t_out3 = 1, w3 = -0.37543/*Випадкові ваги*/;
+    long double t_in4[3] = { 1, 1, 1 }, t_out4 = 1, w4 = -0.37543/*Випадкові ваги*/;
+    //long double t_in5[3] = { 1, 1, 1 }, t_out5 = 1, w5 = -0.37543/*Випадкові ваги*/, out5;
+    for (long i1 = 0; i1 < 21474836464; i1++) {
+        long double out1 = sigmoid(func(t_in1, w1));
+        w1 += func(t_in1, (t_out1 - out1) * out1 * (1 - out1));
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
- 
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        /* pi is partitioning index, arr[p] is now
-        at right place */
-        int pi = partition(arr, low, high);
- 
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+    for (long i2 = 0; i2 < 21474836463; i2++) {
+        long double out2 = sigmoid(func(t_in2, w2));
+        w2 += func(t_in2, (t_out2 - out2) * out2 * (1 - out2));
     }
+    for (long i3 = 0; i3 < 21474836462; i3++) {
+        long double out3 = sigmoid(func(t_in3, w3));
+        w3 += func(t_in3, (t_out3 - out3) * out3 * (1 - out3));
+    }
+    for (long i4 = 0; i4 < 21474836461; i4++) {
+        long double out4 = sigmoid(func(t_in4, w4));
+        w4 += func(t_in4, (t_out4 - out4) * out4 * (1 - out4));
+    }
+    /*for (long i5 = 0; i5 < 21474836460; i5++) {
+        out5 = sigmoid(func(t_in5, w5));
+        w5 += func(t_in5, (t_out5 - out5) * out5 * (1 - out5));
+    }*/
+    long double avearge = average(out1, out2, out3, out4);
+    
+    //std::cout << out1 << std::endl;
+    //std::cout << out2 << std::endl;
+    //std::cout << out3 << std::endl;
+    //std::cout << out4 << std::endl;
+    //std::cout << out5 << std::endl;
+    //std::cout << average << std::endl;
 }
- 
- 
-// Driver Code
-int main()
-{
-   int sz = 100;
-   int randArray[sz];
-   for(int i=0;i<sz;i++)
-      randArray[i]=rand()%2147483645;  //Generate number between 0 to 2147483645
-  
-   
-    int n = sizeof(randArray) / sizeof(randArray[0]);
-    quickSort(randArray, 0, n - 1);
-    return 0;
-}
- 
+
